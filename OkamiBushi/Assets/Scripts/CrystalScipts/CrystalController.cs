02ff8bd786f7f184e3aed3c50170353b8c78a6c4
Unknown <a.rndmized@gmail.com>
2017-12-02 15:23:25 +0000

    Added Scripts for Crystal.

---
+++ OkamiBushi/Assets/Scripts/CrystalScipts/CrystalController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrystalController : MonoBehaviour
+{
+
+    CrystalStats stats;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        stats = GetComponent<CrystalStats>();
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Colliding with: " + other.gameObject.name);
+        if (other.gameObject.name == "EnemySword")
+        {
+            stats.TakeDamage(15);
+            //Debug.Log("Current Health: " + stats.currentHealth);
+        }
+
+    }
+}
+
---
+++ OkamiBushi/Assets/Scripts/CrystalScipts/CrystalManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrystalManager : MonoBehaviour {
+
+    public static CrystalManager instance;
+
+    void Awake()
+    {
+        instance = this;
+    }
+
+    public GameObject crystal;
+}
