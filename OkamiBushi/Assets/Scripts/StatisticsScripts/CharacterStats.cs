1121d589d21e653f55d2e08f0908c046ed00f1f0
Unknown <a.rndmized@gmail.com>
2017-12-02 15:20:50 +0000

    Added Scripts for stats handling

---
+++ OkamiBushi/Assets/Scripts/StatisticsScripts/CharacterStats.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterStats : MonoBehaviour {
+
+    public int maxHealth;
+    public int currentHealth { get; private set; }
+
+    public Stat damage;
+    public Stat armor;
+
+    void Awake()
+    {
+        currentHealth = maxHealth;
+    }
+
+    public void TakeDamage(int damage)
+    {
+
+        damage -= armor.getValue();
+        damage = Mathf.Clamp(damage,0,maxHealth);
+        currentHealth -= damage;
+
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+
+    }
+
+    public virtual void Die()
+    {
+        //Insert death method here
+    }
+}
---
+++ OkamiBushi/Assets/Scripts/StatisticsScripts/PlayerStats.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : CharacterStats {
+
+    public override void Die()
+    {
+        base.Die();
+        //play death animation - destroy gameobject - drop booster
+        //Destroy(gameObject);
+        Animator anim = GetComponent<Animator>();
+        anim.Play("Death", 0);
+    }
+
+
+}
