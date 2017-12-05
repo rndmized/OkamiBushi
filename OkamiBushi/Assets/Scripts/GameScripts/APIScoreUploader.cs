7c2e33f9d602ea7f4c6e562738c2f7cd19bb1c3d
Albert Rando <a.rndmized@gmail.com>
2017-12-05 23:41:17 +0000

    Game can now send request to server to store scores correctly.

--- OkamiBushi/Assets/Scripts/GameScripts/APIScoreUploader.cs
+++ OkamiBushi/Assets/Scripts/GameScripts/APIScoreUploader.cs
@@ -2,23 +2,46 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System;
 
 public class APIScoreUploader : MonoBehaviour
 {
+    string username;
+    Score score;
 
+    private void Start()
+    {
+        score = new Score();
+    }
+    
 
     public void UploadScore()
     {
+        score.playerName = GameObject.FindGameObjectWithTag("DefaultButton").GetComponentInChildren<Text>().text;
+        score.score = ScoreController.score;
         StartCoroutine(Upload());
+        
     }
 
     IEnumerator Upload()
     {
         List<IMultipartFormSection> formData = new List<IMultipartFormSection>();
-        formData.Add(new MultipartFormDataSection("score="+ ScoreController.score));
-        formData.Add(new MultipartFormDataSection("name=" + ScoreController.score));
+        formData.Add(new MultipartFormDataSection("score=" + ScoreController.score));
+        formData.Add(new MultipartFormDataSection("name=" + username));
 
-        UnityWebRequest www = UnityWebRequest.Post("http://rndmized.pythonanywhere.com/score", "string");
+        //UnityWebRequest www = UnityWebRequest.Post("http://rndmized.pythonanywhere.com/score", "string");
+
+        var json = JsonUtility.ToJson(score);
+        Debug.Log(json);
+
+        Dictionary<string, string> formFields = new Dictionary<string, string>();
+        formFields.Add("playerName", score.playerName);
+        formFields.Add("score", score.score.ToString());
+
+        UnityWebRequest www = UnityWebRequest.Post("http://rndmized.pythonanywhere.com/score", formFields);
+
         yield return www.SendWebRequest();
 
         if (www.isNetworkError || www.isHttpError)
@@ -27,9 +50,17 @@ public class APIScoreUploader : MonoBehaviour
         }
         else
         {
+            Debug.Log(www.downloadHandler.text);
             Debug.Log("Form upload complete!");
-            Debug.Log(www.responseCode);
+            //SceneManager.LoadScene("TitleScreenScene");
         }
     }
 
+}
+
+[Serializable]
+public class Score
+{
+    public int score;
+    public string playerName;
 }
