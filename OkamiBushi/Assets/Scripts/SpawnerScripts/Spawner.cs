d2154069a924a3ad44bd15ee0e882a5ec501088a
Unknown <a.rndmized@gmail.com>
2017-12-06 14:12:14 +0000

    Spawner script commented.

--- OkamiBushi/Assets/Scripts/SpawnerScripts/Spawner.cs
+++ OkamiBushi/Assets/Scripts/SpawnerScripts/Spawner.cs
@@ -1,16 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+/* Intantiate GameObjects of a given tipe for a definite amount of time */
 public class Spawner : MonoBehaviour {
 
+    // Game object to spawn reference
     public GameObject enemy;
+    // Spawning variables
     public Vector3 spawnPoint;
     public float spawnTimes;
     public int startTimer;
-
+    // Loop controller
     public bool stop;
 
+    // Initialise values 
     private void Start()
     {
         stop = false;
@@ -19,24 +22,22 @@ public class Spawner : MonoBehaviour {
     }
 
     private void Update()
-    {
-
-        if (Input.GetKeyDown(KeyCode.T))
+    {   
+        /* Testing spawning */
+        /*if (Input.GetKeyDown(KeyCode.T))
         {
             stop = false;
             StartCoroutine(Spawn());
-        }
+        }*/
     }
-
+    // Co - routine once started will keep spawning game objects referenced.
     IEnumerator Spawn()
     {
-
         yield return new WaitForSeconds(startTimer);
         while (!stop)
         {
             Instantiate(enemy,transform.TransformPoint(0,0,0), gameObject.transform.rotation);
             yield return new WaitForSeconds(spawnTimes);
-            //stop = true;
         }
     }
 
