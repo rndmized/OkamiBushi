c5a587debc718ab22efec0a62a4a7de5af2e3651
Unknown <a.rndmized@gmail.com>
2017-12-06 13:28:09 +0000

    Cleaned and commented Camera Script.

--- OkamiBushi/Assets/Scripts/CameraScripts/CameraController.cs
+++ OkamiBushi/Assets/Scripts/CameraScripts/CameraController.cs
@@ -1,54 +1,28 @@
 ﻿using UnityEngine;
 
+
+/* Simple script to handle camera behaviour */
 public class CameraController : MonoBehaviour
 {
 
-
+    /* Get references for player, camera and center point from Unity editor */
     public Transform playerCam, character, centerPoint;
 
-    public float mouseX, mouseY;
-    public float mouseSensitivity = 10f;
-    public float mouseYPosition = 1f;
-
-    public float zoom;
-    public float zoomSpeed = 2;
-
-    public float zoomMin = -2f;
-    public float zoomMax = -10f;
-
-    public float rotationSpeed = 5f;
-
-
-    void Start()
-    {
-
-    }
-
+    /* Determine camera position */
+    public float posX, posY;
+    public float posYPosition = 1f;
 
     void Update()
     {
 
-        //Getting Zoom Variation from Input
-        zoom += Input.GetAxis("Mouse ScrollWheel") * zoomSpeed;
-
-        //Limit Zoom Scope
-        if (zoom > zoomMin)
-            zoom = zoomMin;
-
-        if (zoom < zoomMax)
-            zoom = zoomMax;
-
-        //Set Camera distance on z axis to new zoom. (closer to the payer)
-        playerCam.transform.localPosition = new Vector3(0, 0, zoom);
-
-
         //Limit and set camera rotation based on center point.
-        mouseY = Mathf.Clamp(mouseY, 1f, 60f);
+        posY = Mathf.Clamp(posY, 1f, 60f);
         playerCam.LookAt(centerPoint);
 
-        centerPoint.localRotation = Quaternion.Euler(mouseY, mouseX, 0);
-
-        centerPoint.position = new Vector3(character.position.x, character.position.y + mouseYPosition, character.position.z);
+        /* Calculate local rotation for the center point */
+        centerPoint.localRotation = Quaternion.Euler(posY, poX, 0);
+        /* Move the center point along with the player */
+        centerPoint.position = new Vector3(character.position.x, character.position.y + posYPosition, character.position.z);
 
     }
 }
